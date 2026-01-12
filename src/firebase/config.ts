e80fe07214c2728a696d9d33ec2082395ee6d931
Alex Sawyer <alex@cre8iongroup.com>
2026-01-12 19:59:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-244954263-1293f",
+  "appId": "1:297455895068:web:9b08fb42767150352815d9",
+  "apiKey": "AIzaSyDnlbOBYrLU28hGOTYNfwHM3e9nIUKhYDM",
+  "authDomain": "studio-244954263-1293f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "297455895068"
+};
