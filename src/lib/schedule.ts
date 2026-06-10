b995b134e880658b4d0bbaf5f0fddd76b79c8471
Alex Sawyer <alexsawyer@Alexs-MacBook-Pro-3.local>
2026-06-10 14:17:14 -0400

    fix: remove Mon 11:30 AM and Tue 11:00 AM slots from workshop options

--- src/lib/schedule.ts
+++ src/lib/schedule.ts
@@ -28,7 +28,6 @@ export const availableSlots: ScheduleDay[] = [
     date: '2026-08-10T12:00:00.000Z',
     sessionTypes: ['workshop'],
     times: [
-      { time: '11:30 AM - 12:30 PM', capacity: 10 },
       { time: '02:00 PM - 03:00 PM', capacity: 10 },
       { time: '03:30 PM - 04:30 PM', capacity: 10 },
     ],
@@ -46,7 +45,6 @@ export const availableSlots: ScheduleDay[] = [
     date: '2026-08-11T12:00:00.000Z',
     sessionTypes: ['workshop'],
     times: [
-      { time: '11:00 AM - 12:00 PM', capacity: 10 },
       { time: '03:00 PM - 04:00 PM', capacity: 10 },
     ],
   },
